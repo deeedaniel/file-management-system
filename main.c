8175dfd0f13d130d44404c0b421055364c535b01
Daniel Nguyen <nguyendaniel1312@gmail.com>
2025-12-08 14:45:16 -0800

    Implement open and close file functions

--- main.c
+++ main.c
@@ -13,13 +13,46 @@ void openFile(char* name) {
         printf("Error: File not found.\n");
         return;
     }
-    // Logic to add to openFileTable goes here...
-    printf("File %s opened.\n", name);
+
+    // Check if file is already open
+    for (int i = 0; i < MAX_OPEN_FILES; i++) {
+        if (openFileTable[i].isOpen && openFileTable[i].fileID == index) {
+            printf("Error: File '%s' is already open.\n", name);
+            return;
+        }
+    }
+
+    // Find a free slot in the Open File Table
+    for (int i = 0; i < MAX_OPEN_FILES; i++) {
+        if (!openFileTable[i].isOpen) {
+            openFileTable[i].fileID = index;
+            openFileTable[i].currentPointer = 0;
+            openFileTable[i].isOpen = true;
+            printf("File %s opened successfully.\n", name);
+            return;
+        }
+    }
+
+    printf("Error: Maximum number of open files reached.\n");
 }
 
 void closeFile(char* name) {
-    // Logic to remove from openFileTable goes here...
-    printf("File %s closed.\n", name);
+    int index = searchDirectory(name);
+    if (index == -1) {
+        printf("Error: File not found.\n");
+        return;
+    }
+
+    // Find the file in the Open File Table
+    for (int i = 0; i < MAX_OPEN_FILES; i++) {
+        if (openFileTable[i].isOpen && openFileTable[i].fileID == index) {
+            openFileTable[i].isOpen = false;
+            printf("File %s closed successfully.\n", name);
+            return;
+        }
+    }
+
+    printf("Error: File '%s' is not currently open.\n", name);
 }
 
 int main() {
